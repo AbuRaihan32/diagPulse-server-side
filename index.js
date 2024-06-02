79d40c4237150647b30bdd695fdf89d49984bbf9
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-06-03 05:53:10 +0600

    successfully connected with mongodb

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const express = require('express');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-
-// ! middleware 
+// ! middleware
 app.use(express.json());
 
+console.log(process.env.DB_PASS, process.env.DB_USER);
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fxbdhbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.get('/', (req, res)=>{
-    res.send('DiagPulse Server is Running')
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log(`DiagPulse Server Running In port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("DiagPulse Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`DiagPulse Server Running In port ${port}`);
+});
